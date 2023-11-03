3382c10c3b670bac3c7925e45596f80d8fc325a1
Sergio Castillo <sergio.castillo5392@stu.mtec.edu>
2023-11-03 09:43:32 -0600

    continued on layout of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ul className='nav nav-underline justify-content-center'>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>Nature</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>Video Games</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>Vehicles</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>Appearal</a>
+          </li>
+        </ul>
+        <hr></hr>
       </header>
 
-      <section>
-      
+      <section className='photoGallery'>
+
+        <div className='row'>
+          <p>Placeholder Text</p>
+        </div>
+
+        <div className='row'>
+          <p>Placeholder Text</p>
+        </div>
+
+        <div className='row'>
+          <p>Placeholder Text</p>
+        </div>
+
       </section>
 
-      <section>
-      
-      </section>
-
-      <section>
-      
-      </section>
+      <footer>
+      <ul className='nav nav-underline justify-content-center'>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>Nature</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>Video Games</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>Vehicles</a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>Appearal</a>
+          </li>
+        </ul>
+        <hr></hr>
+        <p id='createrMark'>Project Created With React -- Created by Sergio Castillo -- For a Mountainland Technical School Project</p>
+      </footer>
     </div>
   );
 }
