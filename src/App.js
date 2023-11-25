76e8faccaa84c4025f5448eef8409296917b2473
Sergio Castillo <sergio.castillo5392@stu.mtec.edu>
2023-11-24 17:58:27 -0700

    Added components for main app.js file to be cleaner and easier to read

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css'
+import BannerNav from './components/Banner';
+import FooterNav from './components/Footer';
 import { createContext, useState } from 'react'
 import useAxios from './hooks/useAxios';
 
@@ -31,47 +33,13 @@ function App() {
   return (
     <div className="App">
 
-      <header className="App-header">
-
-        <ul className='nav nav-underline justify-content-center'>
-
-        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Nature')}>Nature</button></li>
-
-        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Video Games')}>Video Games</button></li>
-
-        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Vehicles')}>Vehicles</button></li>
-
-        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Apparel')}>Apparel</button></li>
-
-        </ul>
-
-        <hr></hr>
-
-        <p value={category} onChange={(e) => setCategory(e.target.value)} style={{textAlign: "center", paddingBottom: 20}}>Selected Category: {category}</p>
-
-      </header>
+      <BannerNav handleCategoryClick={handleCategoryClick} category={category} setCategory={setCategory} />
 
       <section className='photoGallery'>
 
-        
-
       </section>;
 
-      <footer>
-        <hr></hr>
-        <ul className='nav nav-underline justify-content-center'>
-
-         <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Nature')}>Nature</button></li>
-
-        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Video Games')}>Video Games</button></li>
-
-        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Vehicles')}>Vehicles</button></li>
-
-        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Apparel')}>Apparel</button></li>
-
-        </ul>
-        <p id='createrMark'>Project Created With React -- Created by Sergio Castillo -- For a Mountainland Technical School Project</p>
-      </footer>
+      <FooterNav handleCategoryClick={handleCategoryClick} category={category} setCategory={setCategory} />
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const BannerNav = ({handleCategoryClick, category, setCategory}) => {
+    return (
+        <header className="App-header">
+
+        <ul className='nav nav-underline justify-content-center'>
+
+        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Nature')}>Nature</button></li>
+
+        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Video Games')}>Video Games</button></li>
+
+        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Vehicles')}>Vehicles</button></li>
+
+        <li><button type='button' className='nav-link' onClick={() => handleCategoryClick('Apparel')}>Apparel</button></li>
+
+        </ul>
+
+        <hr></hr>
+
+        <p value={category} onChange={(e) => setCategory(e.target.value)} style={{textAlign: "center", paddingBottom: 20}}>Selected Category: {category}</p>
+
+      </header>
+    )
+}
+
+export default BannerNav
